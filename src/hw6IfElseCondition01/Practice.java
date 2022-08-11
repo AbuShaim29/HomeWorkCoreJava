f2daed1bd09cab82ebc704220b7be797f91cb588
AbuShaim29 <abushaimqa@gmail.com>
2022-08-11 01:51:18 -0400

    update

---
+++ src/hw6IfElseCondition01/Practice.java
@@ -0,0 +1,37 @@
+package hw6IfElseCondition01;
+
+public class Practice {
+	
+	
+	public static void main(String[] args) {
+		int age =18;
+		
+		
+		
+		if (age==18) {
+			System.out.println("I am a voter");
+			
+			
+			
+		}else if(age<=18) {
+			System.out.println("i  am not a voter");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+}
